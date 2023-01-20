142e56bea4a4b80dbbd919f70d64a0bda535a990
Diogo Santos <diogodeese@gmail.com>
2023-01-20 00:25:19 +0000

    feat: habit days color and progress bar

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import logoImage from "../assets/logo.svg"
 import { Plus, X } from "phosphor-react"
 import * as Dialog from "@radix-ui/react-dialog"
+import { NewHabitForm } from "./NewHabitForm"
 
 export function Header() {
   return(
@@ -23,17 +24,19 @@ export function Header() {
             <Dialog.Portal>
               <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
 
-              <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-                Modal Content
+              <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-xl w-full max-w-md top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+                <Dialog.Title className="text-3xl leading-tight font-bold">
+                  New Habit
+                </Dialog.Title>
 
                 <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
                   <X size={24} aria-label="Close" />
                 </Dialog.Close>
+
+                <NewHabitForm />
               </Dialog.Content>
             </Dialog.Portal>
           </Dialog.Root>
-
-          
         </div>
   )
 }
